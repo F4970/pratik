cba48c197994bdaec61e38ece1d2bbd8c6beb692
F4970 Özge <celebiiozgee@gmail.com>
2022-11-24 12:55:33 +0300

    JavaLearn

---
+++ src/j19_StaticVeriable/BisikletClass.java
@@ -0,0 +1,30 @@
+package j19_StaticVeriable;
+
+public class BisikletClass {
+     /*
+    Task 04->
+     Bisiklet Class :Properties(fields) : hiz, vites
+     method  :hizdegistir(int yeniHiz), vitesArtir()-> 1 değer vites artırıyor,
+     vitesAzalt() -> 1 deger vites azaltıyor, durumGoster -> O andaki hızı ve vitesi gösteriyor
+     vites 5 den büyük ve 1 den küçük olamaz.
+     yukarıdaki methodları seçtirilen islemYap methodu ile Runner class'da obj ile methodları çalıştırınız
+    Trick -> Kullanıcıya metodlarla iligli bir menü veriniz.
+     */
+
+    //Fields
+    int hız;
+    int vıtes;
+
+    public BisikletClass(int hız, int vıtes) {
+        this.hız = hız;
+        this.vıtes = vıtes;
+        
+
+    }
+
+    public void vitesArttır() {
+    }
+
+    public void hızdeğiştir(int yeniHız) {
+    }
+}
---
+++ src/j19_StaticVeriable/BisikletRunner.java
@@ -0,0 +1,7 @@
+package j19_StaticVeriable;
+
+public class BisikletRunner {
+    public static void main(String[] args) {
+
+    }
+}
